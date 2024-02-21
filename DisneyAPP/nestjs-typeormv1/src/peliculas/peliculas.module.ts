fc82898158b940178d4deb4512cb2a87c1c80bf0
Bryan J <bryandavila520@gmail.com>
2024-02-21 09:17:52 +0100

    carpetitas

--- DisneyAPP/nestjs-typeormv1/src/peliculas/peliculas.module.ts
+++ DisneyAPP/nestjs-typeormv1/src/peliculas/peliculas.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PeliculasController } from './controllers/peliculas.controller';
+import { PeliculasService } from './services/peliculas.service';
 
-@Module({})
+@Module({
+  controllers: [PeliculasController],
+  providers: [PeliculasService]
+})
 export class PeliculasModule {}
